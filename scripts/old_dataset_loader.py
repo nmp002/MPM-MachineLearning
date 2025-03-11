2964e9c1f123cb08d2a403eee135908a39b8c964
nmp002 <nmp002@uark.edu>
2025-03-11 15:04:16 -0500

    fixed label_tensor float conversion

--- scripts/old_dataset_loader.py
+++ scripts/old_dataset_loader.py
@@ -83,6 +83,6 @@ class MicroscopyDataset(Dataset):
         label_tensor = torch.tensor(label, dtype=torch.float)
 
         if self.label == 'classification':
-            label_tensor = float(1 if label_tensor >= 25 else 0)
+            label_tensor = torch.tensor(1 if label_tensor >= 25 else 0, dtype=torch.float32)
 
         return combined_image, label_tensor
