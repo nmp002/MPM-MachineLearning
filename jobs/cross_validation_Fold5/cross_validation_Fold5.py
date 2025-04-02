b69f17f29ddc42dbc1a9b50d783ba535fe03fce7
nmp002 <nmp002@uark.edu>
2025-04-02 10:09:53 -0500

    changed epochs to 500 and testing every 50 epochs

--- jobs/cross_validation_Fold5/cross_validation_Fold5.py
+++ jobs/cross_validation_Fold5/cross_validation_Fold5.py
@@ -39,7 +39,7 @@ def set_seed(seed: int = 42) -> None:
 
 # HYPERPARAMETERS
 batch_size = 16
-epochs = 750
+epochs = 500
 learning_rate = 1e-6
 
 # SET SPLITS
@@ -248,7 +248,7 @@ for epoch in range(epochs):
 
 
 
-    if (epoch+1) % 5 == 0:
+    if (epoch+1) % 50 == 0:
         # Save the trained model every 250 epochs
         torch.save(model.state_dict(), f"classification_model_epoch{epoch+1}.pt")
         with torch.no_grad():
