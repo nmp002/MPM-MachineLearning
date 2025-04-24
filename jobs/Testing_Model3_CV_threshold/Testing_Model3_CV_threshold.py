91128bfdeb207256bd25706b4b36cebe432ecfc5
nmp002 <nmp002@uark.edu>
2025-04-24 13:54:44 -0500

    Refactor Model 3 training to simplify dataset and loader logic
    Removed redundant lists and loop logic for models, datasets, and loaders. Streamlined initialization and training by directly operating on single instances of model, optimizer, datasets, and loaders.

--- jobs/Testing_Model3_CV_threshold/Testing_Model3_CV_threshold.py
+++ jobs/Testing_Model3_CV_threshold/Testing_Model3_CV_threshold.py
@@ -92,15 +92,8 @@ def get_indices_by_sample_ids(img_labels, sample_ids_set):
 # INITIALIZE MODELS, OPTIMIZERS, & LOSS FNS
 # =========================================
 
-channel_set = ['shg']   # Model 3
+channels = ['shg']   # Model 3
 
-models = []
-optimizers = []
-loss_fns = []
-datasets = []
-train_datasets = []
-train_loaders = []
-test_loaders = []
 
 train_ids = ['Sample_001', 'Sample_002', 'Sample_003','Sample_004','Sample_014',
             'Sample_005', 'Sample_006', 'Sample_007', 'Sample_008', 'Sample_009',
@@ -111,159 +104,147 @@ train_ids = ['Sample_001', 'Sample_002', 'Sample_003','Sample_004','Sample_014',
 test_ids = ['Sample_010', 'Sample_011', 'Sample_012', 'Sample_013']
 
 
-for channels in channel_set:
-    in_channels = len(channels)
-    model = classificationModel(in_channels=in_channels).to(device)
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=0.01)
-    loss_fn = nn.BCELoss()
 
-    dataset = MicroscopyDataset(
-        data_dir=data_dir,
-        labels_csv=labels_csv,
-        channels=channels,
-        transform=None,
-        label_fn=label_fn
-    )
+in_channels = len(channels)
+model = classificationModel(in_channels=in_channels).to(device)
+optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=0.01)
+loss_fn = nn.BCELoss()
 
-    train_dataset = MicroscopyDataset(
-        data_dir=data_dir,
-        labels_csv=labels_csv,
-        channels=channels,
-        transform=train_transform,
-        label_fn=label_fn
-    )
+dataset = MicroscopyDataset(
+    data_dir=data_dir,
+    labels_csv=labels_csv,
+    channels=channels,
+    transform=None,
+    label_fn=label_fn
+)
 
-    # Create lists of indices for each split
-    train_indices = get_indices_by_sample_ids(train_dataset.img_labels, set(train_ids))
-    test_indices = get_indices_by_sample_ids(dataset.img_labels, set(test_ids))
+train_dataset = MicroscopyDataset(
+    data_dir=data_dir,
+    labels_csv=labels_csv,
+    channels=channels,
+    transform=train_transform,
+    label_fn=label_fn
+)
 
-    # Create dataset subsets
-    train_dataset = Subset(train_dataset, train_indices)
-    test_dataset = Subset(dataset, test_indices)
+# Create lists of indices for each split
+train_indices = get_indices_by_sample_ids(train_dataset.img_labels, set(train_ids))
+test_indices = get_indices_by_sample_ids(dataset.img_labels, set(test_ids))
 
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+# Create dataset subsets
+train_dataset = Subset(train_dataset, train_indices)
+test_dataset = Subset(dataset, test_indices)
 
-    models.append(model)
-    optimizers.append(optimizer)
-    loss_fns.append(loss_fn)
-    datasets.append(dataset)
-    train_datasets.append(train_dataset)
-    train_loaders.append(train_loader)
-    test_loaders.append(test_loader)
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
 
 # ==================================
 # TRAINING LOOP
 # ==================================
 
-for i in range(len(models)):
-    with open(results_file, 'a') as f:
-        f.write(f'\nTraining model_3:\n')
-        f.write(f'Channel Inputs: {channel_set[i]}\n')
 
-    train_losses = []
+with open(results_file, 'a') as f:
+    f.write(f'\nTraining model_3:\n')
+    f.write(f'Channel Inputs: {channels}\n')
 
-    for epoch in range(epochs):
-        if (epoch+1) == 1 or (epoch+1) % 50 == 0:
+train_losses = []
+
+for epoch in range(epochs):
+    if (epoch+1) == 1 or (epoch+1) % 50 == 0:
+        with open(results_file, 'a') as f:
+            f.write(f'Epoch {epoch+1}/{epochs}\n')
+
+    # Train the model
+    model.train()
+    epoch_train_loss = 0.0
+    for x, target, _ in train_loader:
+        x, target = x.to(device), target.to(device)
+        optimizer.zero_grad()
+        out = model(x).squeeze()
+
+        invalid_outs = out[(out < 0) | (out > 1)]
+        if invalid_outs.numel() > 0 or torch.isnan(out).any():
+            print(f'Found invalid model outputs: {invalid_outs}')
+        invalid_targets = target[(target < 0) | (target > 1)]
+        if invalid_targets.numel() > 0:
+            print(f'Found invalid model targets: {invalid_targets}')
+
+
+        loss = loss_fn(out, target)
+        epoch_train_loss += loss.item()
+        loss.backward()
+        optimizer.step()
+
+    avg_train_loss = epoch_train_loss / len(train_dataset)
+    train_losses.append(avg_train_loss)
+
+
+    if (epoch+1) == epochs:
+        torch.save(model.state_dict(), 'Model3_for_Testing.pt')
+        with open(results_file, 'a') as f:
+            f.write('Testing Model 3 with Average CV Threshold...\n')
+
+        model.load_state_dict(torch.load('Model3_for_Testing.pt'))
+
+
+        with torch.no_grad():
+            model.eval()
+            ys, targets = [], []
+            sample_targets = defaultdict(list)
+            for img, target, sample_ids in test_loader:
+                img = img.to(device)
+                y = model(img).squeeze().cpu()
+                y = y.numpy().astype(np.float64).tolist()
+
+                for id_name, target_name in zip(sample_ids, target):
+                    sample_targets[id_name].append(target_name.item())
+
+                target = target.cpu()
+                target = target.numpy().astype(np.float64).tolist()
+                ys.append(y)
+                targets.append(target)
+
+            averaged_targets = {}
+            for sample_id, target_list in sample_targets.items():
+                if all(target == target_list[0] for target in target_list):
+                    averaged_targets[sample_id] = target_list[0]
+                else:
+                    print(f'Warning: Inconsistent targets for {sample_id} -> {target_list}')
+                    epoch = epochs
+
+            ys = [item for y in ys for item in y]
+            sample_ys = np.mean(np.array(ys).reshape(-1, 5), axis=1)
+            threshold = 0.4896
             with open(results_file, 'a') as f:
-                f.write(f'Epoch {epoch+1}/{epochs}\n')
+                f.write(f'Average CV Threshold for Model 3: {threshold}\n')
 
-        model = models[i]
-        optimizer = optimizers[i]
-        loss_fn = loss_fns[i]
-
-        # Train the model
-        model.train()
-        epoch_train_loss = 0.0
-        for x, target, _ in train_loaders[i]:
-            x, target = x.to(device), target.to(device)
-            optimizer.zero_grad()
-            out = model(x).squeeze()
-
-            invalid_outs = out[(out < 0) | (out > 1)]
-            if invalid_outs.numel() > 0 or torch.isnan(out).any():
-                print(f'Found invalid model outputs: {invalid_outs}')
-            invalid_targets = target[(target < 0) | (target > 1)]
-            if invalid_targets.numel() > 0:
-                print(f'Found invalid model targets: {invalid_targets}')
-
-
-            loss = loss_fn(out, target)
-            epoch_train_loss += loss.item()
-            loss.backward()
-            optimizer.step()
-
-        avg_train_loss = epoch_train_loss / len(train_dataset)
-        train_losses.append(avg_train_loss)
-
-
-        if (epoch+1) == epochs:
-            torch.save(model.state_dict(), 'Model3_for_Testing.pt')
-            with open(results_file, 'a') as f:
-                f.write('Testing Model 3 with Average CV Threshold...\n')
-
-            model.load_state_dict(torch.load('Model3_for_Testing.pt'))
-
-
-            with torch.no_grad():
-                model.eval()
-                ys, targets = [], []
-                sample_targets = defaultdict(list)
-                for img, target, sample_ids in test_loaders[i]:
-                    img = img.to(device)
-                    y = model(img).squeeze().cpu()
-                    y = y.numpy().astype(np.float64).tolist()
-
-                    for id_name, target_name in zip(sample_ids, target):
-                        sample_targets[id_name].append(target_name.item())
-
-                    target = target.cpu()
-                    target = target.numpy().astype(np.float64).tolist()
-                    ys.append(y)
-                    targets.append(target)
-
-                averaged_targets = {}
-                for sample_id, target_list in sample_targets.items():
-                    if all(target == target_list[0] for target in target_list):
-                        averaged_targets[sample_id] = target_list[0]
-                    else:
-                        print(f'Warning: Inconsistent targets for {sample_id} -> {target_list}')
-                        epoch = epochs
-
-                ys = [item for y in ys for item in y]
-                sample_ys = np.mean(np.array(ys).reshape(-1, 5), axis=1)
-                threshold = 0.4896
-                with open(results_file, 'a') as f:
-                    f.write(f'Average CV Threshold for Model 3: {threshold}\n')
-
-                t = list(averaged_targets.values())
-                o = sample_ys
-                preds = [out_value >= threshold for out_value in o]
-                conf_matrix = ConfusionMatrixDisplay.from_predictions(t, preds)
-                conf_matrix.plot()
-                plt.savefig(f'Model_3_epoch{epochs}_confusion_matrix_Test_Set.png')
+            t = list(averaged_targets.values())
+            o = sample_ys
+            preds = [out_value >= threshold for out_value in o]
+            conf_matrix = ConfusionMatrixDisplay.from_predictions(t, preds)
+            conf_matrix.plot()
+            plt.savefig(f'Model_3_epoch{epochs}_confusion_matrix_Test_Set.png')
 
 
 
-        # Plot loss curves at specified epochs
-        if (epoch + 1) == epochs:
-            fig, ax = plt.subplots(figsize=(6, 4))
+    # Plot loss curves at specified epochs
+    if (epoch + 1) == epochs:
+        fig, ax = plt.subplots(figsize=(6, 4))
 
-            # Plot raw training loss
-            ax.plot(train_losses, label='Training Loss (Raw)', color='blue', alpha=0.3)
+        # Plot raw training loss
+        ax.plot(train_losses, label='Training Loss (Raw)', color='blue', alpha=0.3)
 
-            # smoothed training loss using a moving average
-            smoothed_train = pd.Series(train_losses).rolling(window=30, min_periods=1).mean()
-            ax.plot(smoothed_train, label='Training Loss (Smoothed)', color='blue', linewidth=2)
+        # smoothed training loss using a moving average
+        smoothed_train = pd.Series(train_losses).rolling(window=30, min_periods=1).mean()
+        ax.plot(smoothed_train, label='Training Loss (Smoothed)', color='blue', linewidth=2)
 
-            ax.set_xlabel('Epoch')
-            ax.set_ylabel('Loss')
-            ax.set_title(f'Model 3 Loss Curve')
-            ax.legend()
-            fig.tight_layout()
-            fig.savefig(f'model_3_loss_epoch{epoch + 1}.png')
-            plt.close(fig)
+        ax.set_xlabel('Epoch')
+        ax.set_ylabel('Loss')
+        ax.set_title(f'Model 3 Loss Curve')
+        ax.legend()
+        fig.tight_layout()
+        fig.savefig(f'model_3_loss_epoch{epoch + 1}.png')
+        plt.close(fig)
 
 
 
