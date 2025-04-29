a345f7379928eaabd8cb87c5f9a7bf2e28a42ba5
nmp002 <nmp002@uark.edu>
2025-04-29 16:36:26 -0500

    Increasing epochs to 3000 in case model needs more time to learn with new updates (normalization, rotation updates)

--- jobs/Method1_Static_Testing_and_Evaluation/Method1_Static_Testing_and_Evaluation.py
+++ jobs/Method1_Static_Testing_and_Evaluation/Method1_Static_Testing_and_Evaluation.py
@@ -63,7 +63,7 @@ set_seed()
 # HYPERPARAMETERS
 # All can be adjusted to monitor and fine-tune model performance
 batch_size = 16 # Number of samples per training batc
-epochs = 1105   # Number of epochs to train
+epochs = 3000   # Number of epochs to train
 learning_rate = 1e-6  # Learning rate for optimization
 weight_decay = 0.01 # Regularization parameter to prevent overfitting
 
