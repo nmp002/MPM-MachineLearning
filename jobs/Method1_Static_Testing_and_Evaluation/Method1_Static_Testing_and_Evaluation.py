b1fc51462d8499bd7dc41506e09663b447dee479
nmp002 <nmp002@uark.edu>
2025-04-30 13:25:38 -0500

    Method 1, Iteration 2: LR = 1e-4, WD = 0.001

--- jobs/Method1_Static_Testing_and_Evaluation/Method1_Static_Testing_and_Evaluation.py
+++ jobs/Method1_Static_Testing_and_Evaluation/Method1_Static_Testing_and_Evaluation.py
@@ -64,7 +64,7 @@ set_seed()
 # All can be adjusted to monitor and fine-tune model performance
 batch_size = 16 # Number of samples per training batc
 epochs = 3000   # Number of epochs to train
-learning_rate = 1e-6  # Learning rate for optimization
+learning_rate = 1e-4  # Learning rate for optimization
 weight_decay = 0.001 # Regularization parameter to prevent overfitting
 
 # Automatically switch between GPU (if available) and CPU
